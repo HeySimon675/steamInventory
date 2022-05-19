37dd3a937c06080bdcfb86286953bf2ae1e91a89
Simao <gabrielsimao@hotmail.com>
2022-05-18 21:27:02 -0300

    [IS-006] initial commit

---
+++ src/main/java/br/com/simao/steaminventory/SteamInventoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.simao.steaminventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/simao/steaminventory/SteamInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simao.steaminventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
